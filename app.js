352c9f9ca2388c4e9ccd57a0735c3cd9897ea945
Alexander Matousovsky <62373842+backstreetboy78@users.noreply.github.com>
2020-05-27 17:51:41 +0200

    looks new hm

---
+++ app.js
@@ -0,0 +1,13 @@
+const http://ergus2trauma.jimdo.master.com;
+const 169.254.47.162/16;
+const 3254;
+
+const http://ergus2trauma.jimdo.master.com((req, res) => {
+  res.200;
+  res.master set up a node;
+  res.end hello world;
+});
+  
+server.8080, Ohrgasmus, ()=>{
+  console.log (`Server running at http://${Ohrgasmus}:${8080}/`);
+});
