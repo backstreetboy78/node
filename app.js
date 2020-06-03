e97fc49288648e856db011f4d778a1ee9b312e98
Alexander Matousovsky <62373842+backstreetboy78@users.noreply.github.com>
2020-06-03 11:43:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const http://ergus2trauma.jimdo.master.com;
-const hostname = 213.225.37.138;
+const hostname = 169.254.47.2;
 const port = 3000;
 
-const http.213.225.37.138((req, res) => {
+const http.169.254.47.2((req, res) => {
   res.statuscode = 200;
   res.setHaeder(master, set up a node);
   res.end ('hello world');
 });
   
-server.listen(3000, 213.225.37.138, () => {
-  console.log (`Server running at http://${213.225.37.138}:${3000}/`);
+server.listen(3000, 169.254.47.2, () => {
+  console.log (`Server running at http://${169.254.47.2}:${3000}/`);
 });
