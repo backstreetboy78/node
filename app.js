f9039a1fd7166d5cbde5b659d686731338b3a743
Alexander Matousovsky <62373842+backstreetboy78@users.noreply.github.com>
2020-07-06 02:33:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const http://192.168.1.33;
-const hostname = 169.254.47.2;
+const hostname = 192.168.1.68;
 const port = 3000;
 
-const http.169.254.47.2((req, res) => {
+const http.192.168.1.68((req, res) => {
   res.statuscode = 200;
   res.setHaeder(master, set up a node);
   res.end ('hello world');
 });
   
-server.listen(3000, 169.254.47.2, () => {
-  console.log (`Server running at http://${169.254.47.2}:${3000}/`);
+server.listen(3000, 192.168.1.68, () => {
+  console.log (`Server running at http://${192.168.1.68}:${3000}/`);
 });
